37623985e0ec48e548438a1d348f61104578613a
Andrii Maliuta <quadr988@gmail.com>
2022-01-03 16:23:24 +0200

    init

--- hello.py
+++ hello.py
@@ -1,5 +1,15 @@
-from flask import Flask, jsonify
+import json, sys
+
+from flask import Flask, jsonify, abort, request, escape, url_for
+from models import db, Cat
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///cat.db"
+db.init_app(app)
+cats = {
+    "cats": [
+        {"name": "Simba", "age": 6}
+    ]
+}
 
 
 @app.route("/")
@@ -9,8 +19,41 @@ def hello():
 
 @app.route("/cats")
 def cats():
-    return jsonify({"cats": [ { "name": "Simba", "age": 6 } ]})
+    # json.dump()
+    return jsonify({})
+
+
+@app.route("/<slug>")
+def cats_slug(slug):
+    try:
+        cat = cats[slug]
+    except KeyError:
+        abort(404)
+    return jsonify(cat)
+
+
+@app.route('/name')
+def hello():
+    name = request.args.get("name", "World")
+    return f'Hello, {escape(name)}!'
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    if "createdb" in sys.argv:
+        with app.app_context():
+            db.create_all()
+        print("Database created!")
+
+    elif "seeddb" in sys.argv:
+        with app.app_context():
+            p1 = Cat(slug="rover", name="Rover",
+                       image_url="http://example.com/rover.jpg")
+            db.session.add(p1)
+            p2 = Cat(slug="spot", name="Spot",
+                       image_url="http://example.com/spot.jpg")
+            db.session.add(p2)
+            db.session.commit()
+        print("Database seeded!")
+
+    else:
+        app.run(debug=True)
